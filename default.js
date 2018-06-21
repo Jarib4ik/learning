479ccfe17e6900ed0831da47481db9e552c39d7b
Andrey Vorobiov <Wizn321@gmail.com>
2018-06-21 11:53:30 +0300

    Complete Home Work

--- default.js
+++ default.js
@@ -14,8 +14,8 @@ function div(a, b) {
     return a / b;
 }
 
-console.log(add(div(50, 5), (mul(100, 50))));
-console.log(div(mul(20, 9), (div(50, 5))));
-console.log(sub(add(56, 35), (mul(9, 5))));
-console.log(mul(div(600, 50), (sub(9, 3))));
-console.log(add( sub( div(30, 2), mul(20, 2)), (div(50, 5))));
+// console.log(add(div(50, 5), (mul(100, 50))));
+// console.log(div(mul(20, 9), (div(50, 5))));
+// console.log(sub(add(56, 35), (mul(9, 5))));
+// console.log(mul(div(600, 50), (sub(9, 3))));
+// console.log(add( sub( div(30, 2), mul(20, 2)), (div(50, 5))));
