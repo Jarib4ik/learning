d87fe73adfe040176c34001e59e4993f814199b1
Andrey Vorobiov <Wizn321@gmail.com>
2018-06-21 17:50:29 +0300

    Changes in function

--- Fibonacci/default.js
+++ Fibonacci/default.js
@@ -1,12 +1,17 @@
 function fibonacci(n) {
     var a = 1;
     var b = 1;
-    for (var i = 3; i <= n; i++) {
-        var c = a + b;
-        a = b;
-        b = c;
-      }
-      return b;
+
+    if (n === 0) {
+        return 0
+    } else {
+        for (var i = 3; i <= n; i++) {
+            var c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 }
 
-console.log(fibonacci(11))
+console.log(fibonacci(8))
