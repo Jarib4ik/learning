fcba3627b27609e67009f97154cd590887729fac
Andrey.Vorobiov <Andrey.vorobiov@myoffice.team>
2018-09-09 22:28:25 +0300

    New version

--- Game/index.js
+++ Game/index.js
@@ -1,48 +1,48 @@
 const buttons = document.querySelector('.flex-container-second');
+const spanVisible = document.querySelector('.flex-container-second .flex-item-second span');
 
 buttons.addEventListener('click', function (e) {
-  e.target.setAttribute('class','flex-item-third')
-});
-
-const butNum = document.querySelector('.flex-container-second .flex-item-second');
-
-butNum.addEventListener('click', function (e) {
-  e.target.setAttribute('class','span-visible')
+  if (e.target.classList.contains('flex-item-second')) {
+    e.target.classList.toggle('flex-item-third')
+  }
 });
 
 
-function randomInteger(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
-}
+function changeNum() {
 
-function randomArr() {
-  const elementSum = document.querySelectorAll('.span-nane').length;
-  let maxNum = elementSum / 2;
-  let arr = []
+  function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
 
-  for (let i = 0; arr.length < maxNum; i++) {
-    rand = randomInteger(1, maxNum);
-    if (arr.indexOf(rand) < 0) {
-      arr.push(rand);
+  function randomArr() {
+    const elementSum = document.querySelectorAll('.span-nane').length;
+    let maxNum = elementSum / 2;
+    let arr = []
+
+    for (let i = 0; arr.length < maxNum; i++) {
+      rand = randomInteger(1, maxNum);
+      if (arr.indexOf(rand) < 0) {
+        arr.push(rand);
+      }
+    }
+    return arr
+  }
+
+  let a = randomArr();
+  let b = randomArr();
+
+  let c = [0, ...a, ...b];
+
+
+  function pushNumbers(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let card = document.querySelector(`.flex-item-second:nth-child(${i}) span`);
+      card.insertAdjacentText(`afterBegin`, arr[i]);
     }
   }
-  return arr
+  pushNumbers(c);
 }
 
-let a = randomArr();
-let b = randomArr();
-
-let c = [0, ...a, ...b];
-
-
-function pushNumbers(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let card = document.querySelector(`.flex-item-second:nth-child(${i}) span`);
-    card.insertAdjacentText(`afterBegin`, arr[i] );
-  }
-}
-
-
-pushNumbers(c);
+changeNum().clearValue
