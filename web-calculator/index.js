5f8722fe5cd5f51a1a37dfde5b5c4965880bb096
Andrey Vorobiov <andrey.vorobiov@myoffice.team>
2018-09-18 16:53:14 +0300

    refactor part 1

--- web-calculator/index.js
+++ web-calculator/index.js
@@ -1,27 +1,22 @@
-const add = (a, b) => a + b;
-const sub = (a, b) => b - a;
-const mul = (a, b) => a * b;
-const div = (a, b) => a / b;
-
 const calculate = {
-    '+': add,
-    '-': sub,
-    '*': mul,
-    '/': div,
+    '+': BasicMath.add,
+    '-': BasicMath.sub,
+    '*': BasicMath.mul,
+    '/': BasicMath.div,
 };
 
 let operatValue = '';
 
 const numButtons = document.querySelector('.numButton');
-const args1 = document.querySelector('.arg1 .itemValue:nth-child(2)');
-const args2 = document.querySelector('.arg2 .itemValue:nth-child(2)');
+const args1 = document.querySelector('.arg:nth-child(2) .itemValue:nth-child(2)');
+const args2 = document.querySelector('.arg:nth-child(4) .itemValue:nth-child(2)');
 const resultValue = document.querySelector('.result .resultValue:nth-child(2)');
 
 let argument1 = '';
 let argument2 = '';
 
 numButtons.addEventListener('click', function (e) {
-    
+
     if (!args1.textContent.length) {
         args1.textContent = e.target.dataset.number;
         argument1 = args1.textContent;
@@ -34,7 +29,6 @@ numButtons.addEventListener('click', function (e) {
             if (args2.textContent.length < 5) {
                 args2.textContent += e.target.dataset.number;
                 argument2 = args2.textContent;
-                debugger
                 const checkFunction = calculate[operatValue];
                 resultValue.textContent = checkFunction(Number(argument1), Number(args2.textContent));
 
@@ -51,13 +45,14 @@ operButtons.addEventListener('click', function (e) {
         if (e.target.dataset.operation === 'C') {
             args1.textContent = '';
             args2.textContent = '';
-            operation.textContent =  '';
+            operation.textContent = '';
             argument1 = '';
             argument2 = '';
             operatValue = '';
             resultValue.textContent = '';
         } else {
-            operation.textContent = e.target.dataset.operation;
+            if (!operation.textContent)
+                operation.textContent = e.target.dataset.operation;
             operatValue = operation.textContent;
         }
     }
